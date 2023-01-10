caf3e23235943891fa336e142f76913bccedf067
racemus <dev@racemus.me>
2023-01-10 19:02:41 +0300

    2244. Minimum Rounds to Complete All Tasks optimization

--- problems/2244. Minimum Rounds to Complete All Tasks/minimumRounds.py
+++ problems/2244. Minimum Rounds to Complete All Tasks/minimumRounds.py
@@ -1,18 +1,18 @@
-import pandas as pd
+from collections import Counter
 
 def minimumRounds(tasks: list[int]) -> int:
-    ''' It uses Pandas, but LeedCode doesn't support it '''
     result = 0
-    tasks_series = pd.Series(tasks)
-    tasks_set = tasks_series.unique()
-    counts = tasks_series.value_counts()
-
-    for i in tasks_set:
-        if counts.get(i) <= 1:
+    # tasks_set = set(tasks)
+    # tasks_dict = {j:tasks.count(j) for j in tasks_set}
+    tasks_dict = Counter(tasks)
+    
+    for k,v in tasks_dict.items():
+        if v <= 1:
             return -1
         else:
-            result += counts.get(i) // 3 + (counts.get(i) % 3 > 0)
-
+            # result += v // 3 + (v % 3 > 0)
+            result += (v + 2) // 3
+    
     return result
 
 print(minimumRounds([2,2,3,3,2,4,4,4,4,4])) # 4
