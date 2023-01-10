8c808d2e82a6697078cca7ddf13db2c4dae07ed5
racemus <dev@racemus.me>
2023-01-10 19:26:13 +0300

    1833. Maximum Ice Cream Bars optimization

--- problems/1833. Maximum Ice Cream Bars/maxIceCream.py
+++ problems/1833. Maximum Ice Cream Bars/maxIceCream.py
@@ -1,17 +1,11 @@
 def maxIceCream(costs: list[int], coins: int) -> int:
-    ''' It iterates through sorted costs, collect bought ice-cream prices and
-        returns its lenght'''
-    if min(costs) > coins:
-        return 0
-    if sum(costs) < coins:
-        return len(costs)
+    ''' It iterates through sorted costs spending coins till it still avaliable '''
     costs.sort()
-    i = 0
-    bought = []
-    while i < len(costs) and sum(bought) <= coins:
-        bought.append(costs[i])
-        i += 1
-    return len(bought[:-1])
+    for i in range(len(costs)):
+        coins -= costs[i]
+        if coins < 0:
+            return i
+    return i + 1
 
 print(maxIceCream([1,3,2,4,1], 7)) # 4 (1,3,2,1)
 print(maxIceCream([10,6,8,7,7,8], 5)) # 0 (cannot afford)
