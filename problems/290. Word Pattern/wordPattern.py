9d6f2e1981badc07a5aeb1dfe954282a8fb1bc40
racemus <dev@racemus.me>
2023-01-10 18:51:47 +0300

    290. Word Pattern optimization

--- problems/290. Word Pattern/wordPattern.py
+++ problems/290. Word Pattern/wordPattern.py
@@ -1,16 +1,19 @@
-def createPattern(input):
-    pattern_list = []
-    pattern_code = []
-
-    for element in input:
-        if element not in pattern_list:
-            pattern_list.append(element)
-        pattern_code.append(pattern_list.index(element))    
-
-    return pattern_code
-
 def wordPattern(pattern: str, s: str) -> bool:
-    return createPattern(pattern) == createPattern(s.split())
+    ''' It puts every pair of letter from pattern and word from s to dictionary in iteration
+        like creating paring rules and compares every new pair with this rules on next iterations '''
+    pattern_dict = {}
+    pattern_list = [*pattern]
+    s_list = s.split()
+    if (len(pattern_list) != len(s_list)) or (len(set(pattern_list)) != len(set(s_list))):
+        return False
+    
+    for i in range(len(pattern_list)):
+        if (pattern_list[i] in pattern_dict) and (pattern_dict[pattern_list[i]] != s_list[i]):
+            return False
+        else:
+            pattern_dict[pattern_list[i]] = s_list[i]
+    
+    return True
 
 print(wordPattern('abba', 'dog cat cat dog')) # True
 print(wordPattern('abba', 'dog cat cat fish')) # False
